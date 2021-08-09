c665367dbf014053f875ae556ab7edf6bb34606f
Tis Lais <tistististis@gmail.com>
2021-08-08 19:19:25 -0700

    Add service to fetch from opdb api

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+export const fetchMachines = async () => {
+  const res = await fetch(`https://opdb.org/api/export/groups?api_token=${process.env.REACT_APP_OPDB_KEY}`);
+  const results = await res.json();
+  return results;
+}
+
+
+export const fetchOpdbMachine = async (term) => {
+  const res = await fetch(`https://opdb.org/api/search?api_token=${process.env.REACT_APP_OPDB_KEY}&q=${term}`);
+
+  return res.json();
+};
+
+export const fetchRkdbMachines = async () => {
+  const res = await fetch('/api/v1/machines');
+
+  return res.json();
+}
